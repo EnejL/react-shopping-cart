4d1901af99b54dbb1e76c1bca3cfb01e2787ea96
enejl <enej.licina@windowslive.com>
2021-03-03 12:20:55 +0100

    small bug fix and styling improvement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,27 @@ import React, { useState } from "react";
 import Modal from './Modal/Modal';
 
 export const Navbar = ({user}) => {
+	const [isActive, setActive] = useState("false");
+
+	const handleToggle = () => {
+		setActive(!isActive);
+		console.log("clicked")
+	};
 
 	const [show, setShow] = useState(false);
-
 	const closeModalHandler = () => setShow(false);
 
 	return (
 		<div className="navbar navbar-inverse navbar-fixed-top">
 			<div className="container">
 				<div className="navbar-header">
-					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<button type="button" className="navbar-toggle" onClick={handleToggle}>
 						<span className="icon-bar"></span>
 						<span className="icon-bar"></span>
 						<span className="icon-bar"></span>
 					</button>
 				</div>
-				<div className="navbar-collapse collapse">
+				<div className={`navbar-collapse ${isActive ? "collapse" : ""}`}>
 					<ul className="nav navbar-nav">
 						<li><a href="/">Home</a></li>
 						<li><a href="#" onClick={() => setShow(true)}>Buy</a></li>
